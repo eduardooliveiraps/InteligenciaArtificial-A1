fec32924886b6b0b84bc8c7c903a77a6e3a3036f
José Miguel Isidro <93340308+zmiguel2011@users.noreply.github.com>
2024-03-13 10:58:25 +0000

    menu functional

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import utils
 import warnings
 import pygame
 import pygame_menu
+import menu
 
 warnings.filterwarnings("ignore", category=UserWarning, module="pygad")
 
@@ -10,49 +11,6 @@ warnings.filterwarnings("ignore", category=UserWarning, module="pygad")
 algorithms = ["Hill Climbing", "Simulated Annealing", "Tabu Search", "Genetic Algorithm"]
 # List of possible input files
 files = ["a_an_example.in.txt", "b_basic.in.txt", "c_coarse.in.txt", "d_difficult.in.txt", "e_elaborate.in.txt"]
-
-# Function to display the title of the program
-def display_title():
-    print("******************************************")
-    print("*    One Pizza - Optimization Problem    *")
-    print("******************************************\n")
-
-
-# Function to select a file from the list of possible input files
-def select_file():
-    print("Select a file:")
-    print("     1. a_an_example.in.txt")
-    print("     2. b_basic.in.txt")
-    print("     3. c_coarse.in.txt")
-    print("     4. d_difficult.in.txt")
-    print("     5. e_elaborate.in.txt")
-    
-    while True:
-        try:
-            choice = int(input("Enter your choice: "))
-            if 1 <= choice <= 5:
-                return f"../data/{files[choice - 1]}"
-            else:
-                print("Invalid choice. Please enter a number between 1 and 5")
-        except ValueError:
-            print("Invalid choice. Please enter a number.")
-
-
-# Function to select an algorithm from the list of possible algorithms
-def select_algorithm():
-    print("Select an algorithm:")
-    for i, algorithm in enumerate(algorithms, 1):
-        print(f"{i}. {algorithm}")
-    
-    while True:
-        try:
-            choice = int(input("Enter your choice: "))
-            if 1 <= choice <= len(algorithms):
-                return choice - 1
-            else:
-                print("Invalid choice. Please enter a number between 1 and", len(algorithms))
-        except ValueError:
-            print("Invalid choice. Please enter a number.")
 
 def set_file(_, choice):
     global file_name
@@ -65,7 +23,6 @@ def set_algorithm(_, choice):
         chosen_algorithm = choice
 
 def start_algorithm():
-    global state, solution, score
     print(f"Running {algorithms[chosen_algorithm]} algorithm...")
     utils.read_input_file(file_name)
 
@@ -82,38 +39,13 @@ def start_algorithm():
     elif chosen_algorithm == 3:
         solution, score = utils.genetic_algorithm()
     
-    state = 1
-
-
-# Initialize the game menu
-def init_menu():
-    menu = pygame_menu.Menu('Welcome', 1000, 700,
-                        theme=pygame_menu.themes.THEME_DARK)
-
-    menu.add.dropselect('File :', [('Example', 0), ('Basic', 1), ('Coarse', 2), ('Dificult', 3), ('Elaborate', 4)], onchange=set_file)
-    menu.add.dropselect('Algorithm :', [('Hill Climbing', 0), ('Simulated Annealing', 1), ('Tabu Search', 2), ('Genetic Algorithm', 3)], onchange=set_algorithm)
-    menu.add.button('Run', start_algorithm)
-    menu.add.button('Quit', pygame_menu.events.EXIT)
-    return menu
-    
-# Draw the final solution and score
-def solution_menu(solution, score):
-    menu = pygame_menu.Menu('Solution', 1000, 700,
-                        theme=pygame_menu.themes.THEME_DARK)
-    menu.add.label(f"Best solution: {solution}")
-    menu.add.label(f"Score: {score}")
-    menu.add.button('Quit', pygame_menu.events.EXIT)
-    return menu
+    return solution, score
     
 
 # Main function
 def main():
-    global state, solution, score
-    state = 0
-    pygame.init()
-    surface = pygame.display.set_mode((1000, 700))
-    menu = init_menu()
-    menu.mainloop(surface)
+    menu_state_manager = menu.MenuStateManager(start_algorithm=start_algorithm, set_file=set_file, set_algorithm=set_algorithm)
+    menu_state_manager.render()
 
 
 main() 
--- src/menu.py
+++ src/menu.py
@@ -2,7 +2,6 @@ import pygame
 import pygame_menu
 from enum import Enum
 
-
 # Define the possible game states using an Enum
 class MenuState(Enum):
     MENU = 0
@@ -23,53 +22,6 @@ class MenuStateManager:
 
         self.current_state = MenuState.MENU
 
-    def start(self):
-        while True:
-            self.handle_events()
-            self.update()
-            self.render()
-
-    # Handle any input events
-    def handle_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.quit_game()
-            elif event.type == pygame.KEYDOWN:
-                if self.current_state == MenuState.MENU:
-                    self.handle_menu_input(event.key)
-                elif self.current_state == MenuState.LOADING:
-                    self.handle_gameplay_input(event.key)
-                elif self.current_state == MenuState.RESULTS:
-                    self.handle_results_input(event.key)
-
-    # Handle input events in the MENU state
-    def handle_menu_input(self, key):
-        if key == pygame.K_1:  # start gameplay
-            self.current_state = MenuState.LOADING
-        elif key == pygame.K_2:  # quit game
-            self.quit_game()
-
-    # Handle input events in the LOADING state
-    def handle_gameplay_input(self, key):
-        if key == pygame.K_p:  # pause
-            self.current_state = MenuState.RESULTS
-
-    # Handle quit input events
-    def handle_quit_input(self, key):
-        if key == pygame.K_q:  # quit game
-            self.quit_game()
-
-    # Handle input events in the RESULTS state
-    def handle_results_input(self, key):
-        if key == pygame.K_r:  # restart game
-            self.current_state = MenuState.MENU
-
-    # Update the game state
-    def update(self):
-        if self.current_state == MenuState.MENU:
-            # game logic and update here
-            pass
-
     # Render the current state
     def render(self):
         self.screen.fill((0, 0, 0))  # clear screen
@@ -85,29 +37,30 @@ class MenuStateManager:
 
     # Render the menu screen
     def render_initial_menu(self):
-        # render menu here
         menu = pygame_menu.Menu('Welcome', 1000, 700,
                         theme=pygame_menu.themes.THEME_DARK)
 
         menu.add.dropselect('File :', [('Example', 0), ('Basic', 1), ('Coarse', 2), ('Dificult', 3), ('Elaborate', 4)], onchange=self.set_file)
         menu.add.dropselect('Algorithm :', [('Hill Climbing', 0), ('Simulated Annealing', 1), ('Tabu Search', 2), ('Genetic Algorithm', 3)], onchange=self.set_algorithm)
-        menu.add.button('Run', self.start_algorithm)
+        menu.add.button('Run', self.render_loading)
         menu.add.button('Quit', pygame_menu.events.EXIT)
 
         menu.mainloop(self.screen)
 
     # Render the gameplay screen
     def render_loading(self):
-        # render gameplay here
-        pass
+        solution, score = self.start_algorithm()
+        self.render_results(solution, score)
+
 
     # Render the game over screen
-    def render_results(self):
-        # render results over here
+    def render_results(self, solution, score):
+        self.start_algorithm()
         menu = pygame_menu.Menu('Solution', 1000, 700,
                         theme=pygame_menu.themes.THEME_DARK)
         menu.add.label(f"Best solution: {solution}")
         menu.add.label(f"Score: {score}")
+        menu.add.button('Back', self.render_initial_menu)
         menu.add.button('Quit', pygame_menu.events.EXIT)
 
         menu.mainloop(self.screen)
@@ -116,9 +69,3 @@ class MenuStateManager:
     def quit_game(self):
         pygame.quit()
         quit()
-
-
-# Entry point of the program
-if __name__ == "__main__":
-    game_state_manager = MenuStateManager()
-    game_state_manager.start()
